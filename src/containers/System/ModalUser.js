2126f55dcbdd08873f19e5054181b83235ead0a2
unknown <huy.do02062004@gmail.com>
2025-08-31 03:37:38 +0700

    video 42 CRUD React 6 - Xóa Users (DELETE) và Sử Dụng Emitter bắn Event | React.JS Cho Người Mới Bắt Đầu

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
+import { emitter } from '../../utils/emitter';
 
 class ModalUser extends Component {
 
@@ -14,9 +15,24 @@ class ModalUser extends Component {
             lastName: "",
             address: ""
         }
+
+        this.listenToEmitter()
+    }
+
+    listenToEmitter() {
+        emitter.on("EVENT_CLEAR_MODAL_DATA", () => {
+            this.setState({
+                email: "",
+                password: "",
+                firstName: "",
+                lastName: "",
+                address: ""
+            })
+        })
     }
 
     componentDidMount() {
+        console.log("mouting modal")
     }
 
     toggle = () => {
@@ -37,7 +53,7 @@ class ModalUser extends Component {
         })
     }
 
-    checkValideInput = () => {
+    checkValidateInput = () => {
         let isValid = true;
         let arrInput = ['email', 'password', 'firstName', 'lastName', 'address']
         for (let i = 0; i < arrInput.length; i++) {
@@ -51,11 +67,10 @@ class ModalUser extends Component {
     }
 
     handleAddNewUser = () => {
-        let isValid = this.checkValideInput()
+        let isValid = this.checkValidateInput()
         if (isValid === true) {
             //call api
             this.props.createNewUser(this.state)
-
         }
     }
 
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import "./UserManage.scss"
-import { getAllUsers, createNewUserService } from "../../services/userService"
+import { getAllUsers, createNewUserService, deleteUserService } from "../../services/userService"
 import ModalUser from './ModalUser';
+import { emitter } from '../../utils/emitter';
 
 class UserManage extends Component {
 
@@ -51,9 +52,22 @@ class UserManage extends Component {
                 this.setState({
                     isOpenModal: false
                 })
+
+                emitter.emit("EVENT_CLEAR_MODAL_DATA")
             }
         } catch (e) {
             console.log(e)
+        }
+    }
+
+    handleDeleteUser = async (user) => {
+        // console.log("click delete", user)
+        let response = await deleteUserService(user.id)
+        if (response && response.errCode === 0) {
+            await this.getAllUsersFromReact()
+        }
+        else {
+            alert(response.errMessage)
         }
     }
 
@@ -105,7 +119,7 @@ class UserManage extends Component {
                                         <td>{item.phonenumber}</td>
                                         <td>
                                             <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
-                                            <button className='btn-delete'><i className="fas fa-trash"></i></button>
+                                            <button className='btn-delete' onClick={() => { this.handleDeleteUser(item) }}><i className="fas fa-trash"></i></button>
                                         </td>
                                     </tr>
                                 )
