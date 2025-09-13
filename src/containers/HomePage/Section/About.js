9a4b1310f7f22923bd7c8247b0bbceb1fcdfcde0
unknown <huy.do02062004@gmail.com>
2025-09-13 15:28:05 +0700

    video 54 Phép Màu Khiến Redux Là Lựa Chọn Number 1 với React App | React - Redux cho người mới bắt đầu

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -16,10 +16,10 @@ class About extends Component {
                         <iframe width="100%" height="400px"
                             src="https://www.youtube.com/embed/2BOvMHv6M_M"
                             title="GIẢI MÃ SỨC MẠNH CÁC ĐỘI TUYỂN TẠI MSI 2025: GEN.G - ỨNG CỬ VIÊN SỐ 1 CHO NGÔI VƯƠNG"
-                            frameborder="0"
+                            frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin"
-                            allowfullscreen>
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen>
                         </iframe>
                     </div>
                     <div className='content-right'>
